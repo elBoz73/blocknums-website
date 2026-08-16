26ba8c9b567085eeb4448bc76234b7633796153e
elBoz73 <elBoz73@users.noreply.github.com>
2026-08-16 19:28:01 +0200

    Deploy site: fix locale links for GitHub Pages base path.
    All locales (fr/en/es/de) now use /blocknums-website/{locale}/ URLs.

---
+++ assets/static/chunks/app/[lang]/privacy/page-b6e74878539edc1e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[632],{881:(e,s,n)=>{Promise.resolve().then(n.bind(n,8099)),Promise.resolve().then(n.bind(n,1495)),Promise.resolve().then(n.bind(n,6621))}},e=>{e.O(0,[509,441,255,358],()=>e(e.s=881)),_N_E=e.O()}]);
